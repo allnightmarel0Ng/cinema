7a04ffba4a1b8f6220a46f99ad2ad9fcfd30fc32
SerzhikWW <zero71362@mail.ru>
2025-06-01 17:36:47 +0300

    добавил in-memory mock, щас буду еще добавлять фичи

--- frontend/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-export const Loader = () => {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Loader2 className="animate-spin h-12 w-12 text-primary" />
-        </div>
-    );
-};
